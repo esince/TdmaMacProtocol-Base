a7112c6b0db7d0d303f4f86cd3f3dcd5506917ad
esince <aydinesi16@itu.edu.tr>
2023-05-04 19:44:27 +0300

    init commit

---
+++ out/gcc-release/src/TDMAMac.o.d
@@ -0,0 +1,6 @@
+out/gcc-release//src/TDMAMac.o: src/TDMAMac.cc src/TDMAMac.h \
+ /home/esince/Downloads/omnetpp-5.6.2/include/omnetpp.h
+
+src/TDMAMac.h:
+
+/home/esince/Downloads/omnetpp-5.6.2/include/omnetpp.h:
